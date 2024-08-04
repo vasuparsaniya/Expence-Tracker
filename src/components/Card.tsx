681130bacefcc3b6b5a6bfcb99a4fb3ceaf08118
vasuparsaniya <vasuparsaniya21@gmail.com>
2024-08-04 18:02:13 +0530

    composition

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/css/Card.css";
+
+//--composition
+function Card(props:any) {
+  const classes = "card " + props.className;
+  return <div className={classes}>
+    {props.children}
+  </div>
+}
+
+export default Card;
--- src/components/ExpenceItem.tsx
+++ src/components/ExpenceItem.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import "../assets/css/ExpenceItem.css";
 import { ExpenceItemProps } from "../types/components/ExpenceItem";
 import ExpenceDate from "./ExpenceDate";
+import Card from "./Card";
 
 function ExpenceItem({id,title,date,amount}:ExpenceItemProps) {
-  return <div className="expense-item">
+  return <Card className="expense-item">
     <ExpenceDate date={ date} />
     <div className="expense-item__description">
       <h2>{title}</h2>
       <div className="expense-item__price">{amount}</div>
    </div>
-  </div>
+    </Card>
 }
 
 export default ExpenceItem
